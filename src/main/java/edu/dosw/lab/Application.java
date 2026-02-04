95c39c2c8719b251c31c942fe5f4adc583e1e1c0
Maria Juliana Rodriguez Caicedo <majuliroca@gmail.com>
2026-02-04 17:53:17 -0500

    Se agrego el main del reto1 y se edito Application

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,8 +1,13 @@
 package edu.dosw.lab;
 
+import edu.dosw.lab.creacionales.reto1.Reto1Main;
+
 public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Proyecto Maven configurado y corriendo correctamente");
-	}
+
+		// Ejecutar Reto 1
+		Reto1Main.ejecutar();
+
 }
---
+++ src/main/java/edu/dosw/lab/creacionales/reto1/Reto1Main.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.creacionales.reto1;
+
+public class Reto1Main {
+
+    public static void ejecutar() {
+        System.out.println("Ejecutando Reto 1 - Tienda Don Pepe...");
+        TiendaDonPepe.ejecutar();
+    }
+
+    public static void main(String[] args) {
+        ejecutar();
+    }
+}
