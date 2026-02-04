dc3bb512401bf644590d9a1a11fc8b9b75b07f2a
Kevyn Forero <kevynforero@gmail.com>
2026-02-04 18:35:32 -0500

    Se añadio el reto2

---
+++ src/main/java/edu/dosw/lab/creacionales/reto2/Reto2Main.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.creacionales.reto2;
+
+public class Reto2Main {
+
+    public static void ejecutar() {
+        System.out.println("Ejecutando Reto 2 - El Chef de 5 Estrellas...");
+        Chef.ejecutar();
+    }
+
+    public static void main(String[] args) {
+        ejecutar();
+    }
+}
