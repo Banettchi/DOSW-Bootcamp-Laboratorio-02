f107d154c9ca6f5023f927befba55476b25a5418
Maria Juliana Rodriguez Caicedo <majuliroca@gmail.com>
2026-02-04 18:34:16 -0500

    Actualizacion Application con el reto2

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,14 +1,17 @@
 package edu.dosw.lab;
 
 import edu.dosw.lab.creacionales.reto1.Reto1Main;
+import edu.dosw.lab.creacionales.reto2.Reto2Main;
 
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Proyecto Maven configurado y corriendo correctamente");
+		System.out.println("Proyecto Maven configurado y corriendo correctamente\n");
 
 		// Ejecutar Reto 1
 		Reto1Main.ejecutar();
 
+		// Ejecutar Reto 2
+		Reto2Main.ejecutar();
+	}
 }
-    }
