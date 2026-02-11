0002ce505ac02c9ead3f0ec2235975a867d9d38b
Maria Juliana Rodriguez Caicedo <majuliroca@gmail.com>
2026-02-11 15:16:40 -0500

    se sube reto 6

---
+++ src/main/java/edu/dosw/lab/comportamiento/reto6/Reto6Main.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.comportamiento.reto6;
+
+public class Reto6Main {
+
+    public static void ejecutar() {
+        System.out.println("Ejecutando Reto 6 - Habla con Soporte Técnico...");
+        SoporteT.ejecutar();
+    }
+
+    public static void main(String[] args) {
+        ejecutar();
+    }
+}
---
+++ src/main/java/edu/dosw/lab/comportamiento/reto6/SoporteNivel2.java
@@ -0,0 +1,11 @@
+package edu.dosw.lab.comportamiento.reto6;
+
+/**
+ * Técnico Intermedio: maneja tickets de nivel INTERMEDIO con cualquier prioridad.
+ */
+public class SoporteNivel2 extends ManejadorSoporte {
+
+    public SoporteNivel2() {
+        super("Técnico Intermedio", Ticket.Nivel.INTERMEDIO, Ticket.Prioridad.ALTA);
+    }
+}
